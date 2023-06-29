d7c6bd3fe9044dc1c37fa3f1e56aef3543bca1ef
Tâm Trần Thiện <tamtt1@firegroup.io>
2023-06-29 10:38:26 +0700

    init commit

---
+++ src/background.ts
@@ -0,0 +1,18 @@
+//  "icons": {
+//     "16": "icons/icon.png",
+//     "32": "icons/icon.png",
+//     "48": "icons/icon.png",
+//     "64": "icons/icon.png",
+//     "128": "icons/icon.png"
+// },
+// "chrome_url_overrides": {
+//     "newtab": "index.html"
+// },
+// "permissions": [
+//     "storage",
+//     "unlimitedStorage"
+// ],
+// "content_security_policy": {
+//     "extension_pages": "scripts-src 'self'; object-src 'self';"
+// },
+// "minimum_chrome_version": "107"
